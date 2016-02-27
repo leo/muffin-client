87e64130caa1798105aed5bc480c38873c52f93c
Mr. Leo <leo@users.noreply.github.com>
2016-02-27 19:17:13 +0100

    Disable hinting
    We’ll handle this in a better way

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,8 @@
-/*jshint node:true*/
-/* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    hinting: false,
     outputPaths: {
       app: {
         js: '/assets/app.js',

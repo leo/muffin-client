094a94a3b9f6d5084cab7642c0a91931a89a278c
Mr. Leo <leo@users.noreply.github.com>
2016-02-27 14:30:06 +0100

    Disable sourcemaps and compress CSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sourcemaps: {
+      enabled: false
+    },
+    sassOptions: {
+      outputStyle: 'compressed',
+      sourceMap: false
+    },
+    fingerprint: {
+      enabled: false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated

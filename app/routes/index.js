37cb2cc514024da8fffa6c8bc9ef138aca4d2b6f
Mr. Leo <leo@users.noreply.github.com>
2016-02-28 19:35:29 +0100

    Require login to access dashboard

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
+import AuthenticatedRoute from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRoute, {
   pageTitle: 'Dashboard'
-});
+})

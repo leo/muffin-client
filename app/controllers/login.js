2fc96b25ac94a77d04b7b540980b39b396b0b9d8
Mr. Leo <leo@users.noreply.github.com>
2016-02-28 19:53:01 +0100

    Send credentials to the server

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,12 +2,34 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+
   actions: {
     authenticate() {
-      let { identification, password } = this.getProperties('identification', 'password')
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason)
+      if($('form').hasClass('shake')) {
+        return
+      }
+
+      let { username, password } = this.getProperties( 'username', 'password' )
+
+      this.get('session').authenticate('authenticator:oauth2', username, password).catch(reason => {
+        var timeout
+
+        clearTimeout(timeout)
+        this.set('loginStatus', 'shake')
+
+        $('input').addClass('wrong')
+
+        timeout = setTimeout(function () {
+          this.set('loginStatus', '')
+        }.bind(this), 1000)
+
+        this.set('errorMessage', reason.error)
       })
     }
-  }
+  },
+
+  checkError: function( top, which ) {
+    var type = which === 'password' ? 'password' : 'text';
+    $( 'input[type="' + type + '"]' ).removeClass( 'wrong' );
+  }.observes( 'username', 'password' )
 })

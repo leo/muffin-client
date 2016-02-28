fdd51a70ecf7bfef2a31280f9148d598430cb407
Mr. Leo <leo@users.noreply.github.com>
2016-02-28 20:37:14 +0100

    Route for resetting password is back

--- app/routes/reset-password.js
+++ app/routes/reset-password.js
@@ -1,13 +1,13 @@
 import Ember from 'ember'
-import AuthenticatedRoute from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
-export default Ember.Route.extend(AuthenticatedRoute, {
-  pageTitle: 'Dashboard',
+export default Ember.Route.extend({
+  pageTitle: 'Reset Password',
+  classNames: ['login'],
   parentController: Ember.computed(function () {
     return this.controllerFor('application')
   }),
   setupController: function (controller, model) {
     this._super(controller, model)
-    this.get('parentController').set('outer', false)
+    this.get('parentController').set('outer', true)
   }
 })

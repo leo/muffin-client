943a05ac15d5ffe936d11dde83aef06dcbd22cb1
Mr. Leo <leo@users.noreply.github.com>
2016-02-28 19:53:50 +0100

    Remove spaces

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -28,8 +28,8 @@ export default Ember.Controller.extend({
     }
   },
 
-  checkError: function( top, which ) {
-    var type = which === 'password' ? 'password' : 'text';
-    $( 'input[type="' + type + '"]' ).removeClass( 'wrong' );
-  }.observes( 'username', 'password' )
+  checkError: function (top, which) {
+    var type = which === 'password' ? 'password' : 'text'
+    $('input[type="' + type + '"]').removeClass('wrong')
+  }.observes('username', 'password')
 })

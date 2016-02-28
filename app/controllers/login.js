76210d0ea3d8fd0c9a5cd0ac2910078da23d3203
Mr. Leo <leo@users.noreply.github.com>
2016-02-28 19:47:02 +0100

    Render login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
-
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password')
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,5 +1,12 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  pageTitle: 'Login'
+  pageTitle: 'Login',
+  parentController: Ember.computed(function () {
+    return this.controllerFor('application')
+  }),
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    this.get('parentController').set('outer', true);
+  }
 })

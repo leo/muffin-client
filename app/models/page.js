748b3ab1a72956f8f6f7e1ff941174ed08d2a6f4
Leo <leo@users.noreply.github.com>
2016-02-29 22:34:28 +0100

    Improved model and listing

--- app/models/page.js
+++ app/models/page.js
@@ -1,17 +1,12 @@
 import DS from 'ember-data'
 
-const attr = DS.attr
-
 export default DS.Model.extend({
-  title: attr('string'),
-  author: attr('string'),
+  title: DS.attr('string'),
+  author: DS.attr('string'),
   dates: {
-    created: attr('date'),
-    updated: attr('date')
+    created: DS.attr('date'),
+    updated: DS.attr('date')
   },
-  content: attr('string'),
-  slug: {
-    type: attr('string'),
-    lowercase: true
-  }
+  content: DS.attr('string'),
+  slug: DS.attr('string')
 })

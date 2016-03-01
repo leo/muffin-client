a3f51096874abd02d40b25eabdfef8c77b38a28f
Leo <leo@users.noreply.github.com>
2016-03-01 13:09:17 +0100

    Page should be wrapped in admin template

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,11 +1,19 @@
 import Ember from 'ember'
+import AuthenticatedRoute from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRoute, {
   pageTitle: 'Pages',
-  renderTemplate: function () {
+  renderTemplate () {
     this.render('list')
   },
   model () {
     return this.store.findAll('page')
+  },
+  parentController: Ember.computed(function () {
+    return this.controllerFor('application')
+  }),
+  setupController: function (controller, model) {
+    this._super(controller, model)
+    this.get('parentController').set('outer', false)
   }
 })

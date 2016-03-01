a36519db38ccdb3187d64663b9c4943355d29094
Leo <leo@users.noreply.github.com>
2016-03-01 12:08:06 +0100

    Authorize correctly

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,5 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace: 'api',
-  authorizer: 'authorizer:application'
+  authorizer: 'authorizer:token'
 })

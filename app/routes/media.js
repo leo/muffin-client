10e9c1e5ba90a79e00e22d65952bc0865096b2b5
Leo <leo@users.noreply.github.com>
2016-03-01 20:57:07 +0100

    Wrap files into application template

--- app/routes/media.js
+++ app/routes/media.js
@@ -2,5 +2,12 @@ import Ember from 'ember'
 import Authenticated from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
 export default Ember.Route.extend(Authenticated, {
-  pageTitle: 'Media'
+  pageTitle: 'Media',
+  parentController: Ember.computed(function () {
+    return this.controllerFor('application')
+  }),
+  setupController (controller, model) {
+    this._super(controller, model)
+    this.get('parentController').set('outer', false)
+  }
 })

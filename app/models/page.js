e3e476f5d336c8554967f67d3ee3f2c0c744e0db
Leo <leo@users.noreply.github.com>
2016-03-01 18:14:02 +0100

    A single date instead of two

--- app/models/page.js
+++ app/models/page.js
@@ -3,10 +3,7 @@ import DS from 'ember-data'
 export default DS.Model.extend({
   title: DS.attr('string'),
   author: DS.attr('string'),
-  dates: {
-    created: DS.attr('date'),
-    updated: DS.attr('date')
-  },
+  updatedAt: DS.attr('date'),
   content: DS.attr('string'),
   slug: DS.attr('string')
 })

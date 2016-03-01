bf551be3fef5f74b7a7029eb21239b6771b5f7f2
Leo <leo@users.noreply.github.com>
2016-03-01 20:56:46 +0100

    Users need to authenticate to see media & users

--- app/routes/media.js
+++ app/routes/media.js
@@ -1,5 +1,6 @@
 import Ember from 'ember'
+import Authenticated from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(Authenticated, {
   pageTitle: 'Media'
 })
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,7 @@
 import Ember from 'ember'
+import Authenticated from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(Authenticated, {
   pageTitle: 'Users',
   renderTemplate () {
     this.render('list')

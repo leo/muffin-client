82298b992da4dbf84c1c93815522f72371be187e
Leo <leo@users.noreply.github.com>
2016-03-01 13:13:57 +0100

    Login and pw reset shouldn't be accessed when logged in

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,7 +1,7 @@
 import Ember from 'ember'
-import AuthenticatedRoute from 'ember-simple-auth/mixins/authenticated-route-mixin'
+import Authenticated from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
-export default Ember.Route.extend(AuthenticatedRoute, {
+export default Ember.Route.extend(Authenticated, {
   pageTitle: 'Pages',
   renderTemplate () {
     this.render('list')

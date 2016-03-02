81ce2df1579162c88fa579180c83781e926cce24
Leo <leo@users.noreply.github.com>
2016-03-02 16:20:37 +0100

    Not needed

--- app/routes/pages/edit.js
+++ app/routes/pages/edit.js
@@ -2,13 +2,8 @@ import Ember from 'ember'
 import Authenticated from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
 export default Ember.Route.extend(Authenticated, {
-  pageTitle: Ember.computed(function () {
-    return 'Test'
-  }),
+  pageTitle: 'Test',
   renderTemplate () {
     this.render('edit')
-  },
-  model (params) {
-    return this.store.findRecord('page', params.page_id);
   }
 })

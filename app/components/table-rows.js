e47f2fd85aeff2de0302945bb3642493e2efe025
Leo <leo@users.noreply.github.com>
2016-03-03 19:45:49 +0100

    Auto-generate table head

--- app/components/table-rows.js
+++ app/components/table-rows.js
@@ -2,5 +2,25 @@ import Ember from 'ember'
 
 export default Ember.Component.extend({
   tagName: '',
-  rows: ['Title', 'Author', 'Date']
+  rows: ['Title', 'Author', 'Date'],
+  init: function () {
+    this._super()
+    const first = this.data.toArray().shift()
+
+    const titles = {
+      title: 'Title',
+      updatedAt: 'Date',
+      author: 'Author'
+    }
+
+    var rows = []
+
+    for (var title in titles) {
+      if (first.get(title)) {
+        rows.push(titles[title])
+      }
+    }
+
+    this.set('rows', rows)
+  }
 })

501639b520677832f0814c3dbb81e72ed3462172
Leo <leo@users.noreply.github.com>
2016-03-03 20:12:56 +0100

    Shouldn't be a separate route

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ Router.map(function() {
   this.route('login');
   this.route('reset-password');
   this.route('pages', function() {
-    this.route('edit', { path: '/:page_id' } );
+    this.route('edit', { path: '/:page_id' });
   });
   this.route('users', function() {
     this.route('edit', { path: '/:user_id' });

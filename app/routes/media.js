02d7e11b7a1a5f8314ca79ec610882e24d0cd768
Leo <leo@users.noreply.github.com>
2016-03-04 20:50:58 +0100

    Handle file uploading differently

--- app/routes/media.js
+++ app/routes/media.js
@@ -1,6 +1,33 @@
 import Ember from 'ember'
 import Authenticated from 'ember-simple-auth/mixins/authenticated-route-mixin'
 
+const { get, set } = Ember
+
 export default Ember.Route.extend(Authenticated, {
-  pageTitle: 'Media'
+  pageTitle: 'Media',
+  actions: {
+    uploadImage (file) {
+      const properties = ['type', 'size', 'name']
+      var details = {}
+
+      for (var property of properties) {
+        details[property] = get(file, property)
+      }
+
+      const record = this.store.createRecord('file', details)
+
+      file.read().then(url => {
+        if (get(record, 'url') == null) {
+          set(record, 'url', url)
+        }
+      })
+
+      file.upload('/api/upload').then(response => {
+        set(record, 'url', response.headers.Location)
+        return record.save()
+      }, () => {
+        record.rollback()
+      })
+    }
+  }
 })

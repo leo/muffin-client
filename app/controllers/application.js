768e5476bd9c19f88be1b7bf5ae33403d3788de5
Leo <leo@users.noreply.github.com>
2016-03-22 16:51:42 +0100

    Toggle menu after transition

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,14 @@ import Ember from 'ember'
 
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
+  checkMenuState: function () {
+    const toggler = $('#chest .toggle')
+
+    if (toggler.hasClass('on')) {
+      $('#chest nav').slideToggle(300)
+      toggler.toggleClass('on')
+    }
+  }.observes('currentPath'),
   actions: {
     logout() {
       this.get('session').invalidate()
